ac567f016dccb09cbd7fadef311913816c710fba
BurBOh <burbohdan64@gmail.com>
2022-04-04 20:19:42 +0300

    make it better

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+import sys
+import logging
  
 app = Flask (__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route ('/')
@@ -16,4 +20,4 @@ def demo1():
     return render_template('Demo1.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
